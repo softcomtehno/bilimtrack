928a5e2a4a9630d2eaa8cd331b27f201029f91d9
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2025-09-17 16:43:50 +0600

    feat: added perfomance for student

--- src/entities/subject/subject.api.ts
+++ src/entities/subject/subject.api.ts
@@ -1,11 +1,16 @@
-import $api from '@/shared/api'
+import $api from '@/shared/api';
 
 export function getSubjects() {
-  return $api.get(`subjects`)
+  return $api.get(`subjects`);
 }
 export function getSubjectsMentor() {
-  return $api.get(`mentor/subjects/me/`)
+  return $api.get(`mentor/subjects/me/`);
 }
 export function getSubjectsStudent() {
-  return $api.get(`subjects/me/`)
+  return $api.get(`subjects/me/`);
+}
+export function getPerformanceBySubject(subjectId: number) {
+  return $api.get(`performance-chart/me/`, {
+    params: { subjectId },
+  })
 }
